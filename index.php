a7d325b0972f3fc210ed0e073a19c76c9a90364f
Luptupulus <luptupulus@hotmail.com>
2016-03-11 14:12:04 +0100

    cambios

---
+++ index.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="./css/estilos.css" media="screen"/>
+    <link rel="shortcut icon" href="black-catFavicon/favicon.ico">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css"
+          integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+
+    <title>AlbertoNieto.com</title>
+
+</head>
+<body background="fotos/fondopag3.jpg">
+<h1>Página personal de Alberto Nieto Gochicoa</h1>
+
+<table style="margin: auto">
+
+    <tr>
+        <th><a href="https://www.google.es/"> GOOGLE.ES </a></th>
+        <th><a href="https://www.linkedin.com/in/alberto-nieto-gochicoa-75896b96"> Linked IN </a></th>
+        <th><a href="https://www.vitoria-gasteiz.org/we001/was/we001Action.do?accionWe001=ficha&accion=home"> Vitoria -
+                Gasteiz </a></th>
+        <th><a href="http://finofilipino.org">Fino Filipino</a></th>
+        <th><a href="http://xataka.com">Xataka</a></th>
+
+    </tr>
+
+</table>
+<br/>
+<br/>
+
+<p>
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-4" , style="margin-left: -2cm">
+
+            <titulo>Datos personales</titulo>
+            <br/>
+            <br/>
+            <ul class="listaDatos" style="border: dashed">
+                <li>Nombre y apellidos: Alberto Nieto Gochicoa</li>
+                <li>fecha de nacimiento: 7 de Junio de 1981</li>
+                <li>Lugar de nacimiento: Vitoria - Gasteiz</li>
+                <li>E-Mail: albertonieto07@hotmail.com</li>
+                <li>Carnet de conducir: A, B1, Disponibilidad de vehículo</li>
+                <li>Otros carnets: Licencia federativa de embarcaciones a vela y a motor, Carnet de carretilla
+                    elevadora, título de
+                    buceo PADI "open water", carnet de grúa torre.
+                </li>
+            </ul>
+
+        </div>
+        <div class="col-sm-8" style="border-bottom: none">
+            <titulo>Experiencia laboral</titulo>
+            <br/>
+            <br/>
+            <ul class="listaDatos">
+                <li><strong>Area funcional:</strong> COMERCIAL DE TELEFONIA.<br/>
+                    <strong>Empresa: </strong> CRSCHANNEL<br/>
+                    <strong>Período:</strong> Enero 2014 – abril 2014.<br/>
+                    <strong>Funciones:</strong> Captar nuevos clientes y promocionar los productos de la empresa
+                    productos.<br/>
+                    <br/>
+                </li>
+                <li><strong>Área funcional:</strong> REPARTIDOR Y COMERCIAL DE PRODUCTOS IBÉRICOS.<br/>
+                    <strong>Empresa:</strong> LA VENTA ÁMSTERDAM.<br/>
+                    <strong>Período:</strong> Mayo 2011 – Octubre 2011. Ámsterdam.<br/>
+                    <strong>Funciones:</strong> Repartir productos ibéricos en restaurantes y comercios en el País de
+                    Holanda. Buscar nuevos clientes y promocionar nuevos productos.<br/>
+                    <br/>
+                </li>
+                <li><strong>Área funcional:</strong> CAMARERO.<br/>
+                    <strong>Empresa:</strong> RESTAURANTE PATA NEGRA.<br/>
+                    <strong>Período:</strong> Noviembre 2010 – Mayo 2011. Ámsterdam.<br/>
+                    <strong>Funciones:</strong> Preparar el comedor y servir mesas y terraza (en español, inglés y
+                    holandés).<br/>
+                    <br/>
+                </li>
+                <li><strong>Área funcional:</strong> GRUISTA Y ENCARGADO DE OBRA.<br/>
+                    <strong>Empresas:</strong> Empresas: Construcciones URTUBI SL.<br/>
+                    <strong>Período:</strong> Marzo 2008 – Julio 2010.<br/>
+                    <strong>Funciones:</strong> Organización de todas las partes de la obra. Supervisión de los grupos
+                    de trabajo y coordinación de gremios. Replanteo de viviendas. Control y mantenimiento del
+                    almacén.<br/>
+                    <br/>
+                </li>
+                <li><strong>Área funcional:</strong> GRUISTA Y OFICIAL DE PRIMERA.<br/>
+                    <strong>Empresa:</strong> Construcciones UBIS SA.<br/>
+                    <strong>Período:</strong> Septiembre 2004 – Febrero 2008.<br/>
+                    <strong>Funciones:</strong> Replanteo de viviendas. Suministrar material con la grúa en planta.
+                    Construcción de tabiques en las viviendas. Carga y descarga de materiales y mantenimiento de
+                    almacén.<br/>
+                    <br/>
+                </li>
+                <li><strong>Área funcional:</strong> PEÓN DE OBRA.<br/>
+                    <strong>Empresa:</strong> Construcciones UBIS SA.<br/>
+                    <strong>Período:</strong> Septiembre 2002 – Agosto 2004.<br/>
+                    <strong>Funciones:</strong> Descargar y cargar camiones de obra. Limpieza y mantenimiento de
+                    instalaciones. Suministrar material al equipo de trabajo.<br/>
+                    <br/>
+                </li>
+                <li>
+                    <strong>Área funcional:</strong> MECÁNICO.<br/>
+                    <strong>Empresa:</strong> ARME VEHICULOS INDUSTRIALES S.A. C.O. DE FIAT.<br/>
+                    <strong> Período:</strong> Septiembre 2000 – Septiembre 2002.<br/>
+                    <strong>Funciones:</strong> Mantenimiento y reparación de vehículos.<br/>
+                    <br/>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+</div>
+</p>
+
+<div style="margin: 2cm">
+    <p>
+        <titulo><strong>FORMACIÓN</strong></titulo>
+
+    <ul>
+        <li>Titulación: Técnico en electromecánica de vehículos. (I.E.S. Mendizabala Junio2000)</li>
+
+        <li> Actualmente, cursando “Grado superior de Desarrollo de sistemas multiplataforma”. (I.E.S. Arriaga 2016)
+        </li>
+    </ul>
+    </p>
+</div>
+
+<div style="margin: 2cm">
+    <p>
+        <titulo><strong>DATOS DE INTERÉS</strong></titulo>
+        <br/>
+        Excelentes actitudes de trabajo y habilidades sociales. Muy responsable, dedicado y buen trabajador en equipo.
+        Comprometido y leal con el trabajo.
+        Habilidad y experiencia en el uso de múltiples tipos de vehículos y maquinaria de construcción como máquinas
+        elevadoras y grúas.
+        Buen manejo de la informática.
+
+
+    </p>
+</div>
+
+
+<br/>
+
+<p style="margin-left: 2cm">
+    <titulo>Aficiones</titulo>
+<div class="container" style="border-bottom: none">
+    <div class="row" style="border-bottom: none">
+        <div class="col-sm-3" , style="margin-left: -2cm">
+            <ul>Natacion</ul>
+        </div>
+        <div class="col-sm-3" , style="margin-left: -2cm">
+            <ul>Snowboard</ul>
+        </div>
+        <div class="col-sm-3" , style="margin-left: -2cm">
+            <ul>surf</ul>
+        </div>
+        <div class="col-sm-3" , style="margin-left: -2cm">
+            <ul>Viajar</ul>
+        </div>
+    </div>
+</div>
+
+</p>
+<br/>
+<p>
+    <titulo><a href="#">Pulsa Aqui para obtener más info</a></titulo>
+</p>
+<br/>
+
+
+<img id="foto1" src="fotos/Foto%2023-1-16%2014%2052%2003.jpg">
+<img id="foto2" src="fotos/Foto%2024-1-16%2021%2018%2016.jpg">
+<img id="foto3" src="fotos/surf.jpeg">
+<img id="foto4" src="fotos/Snowboard-1.jpg">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js"
+        integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7"
+        crossorigin="anonymous"></script>
+</body>
+</html>
